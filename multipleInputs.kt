d85c2863971c6d8db84e1a0de7f1aa587b333605
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-07-17 19:21:57 -0700

    Create multipleInputs.kt

---
+++ multipleInputs.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var e = 0
+    while(true) {
+        var input = readLine()!!.toInt()
+        if(input==0){
+            break
+        }
+        e++
+    }
+    println(e)
+}
