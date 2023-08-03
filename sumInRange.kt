9abde0d32f6569be610ed83361996747da973994
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-08-02 20:03:37 -0700

    Create sumInRange.kt

---
+++ sumInRange.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val x = readLine()!!.toInt()
+    val y = readLine()!!.toInt()
+    var e = 0
+    for(g in x..y){
+        e+= g
+        }
+        println(e)
+}
